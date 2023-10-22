3b9f365dd322f520e7e143ec4c1b6a340f2f5952
MikhailV89 <114851579+MikhailV89@users.noreply.github.com>
2023-10-22 17:45:36 +0300

    Add files via upload

---
+++ hw_4/index.js
@@ -0,0 +1,14 @@
+let num1 = +prompt('Write first number?');
+let num2 = +prompt('Write second number?');
+
+let sum = num1 + num2;
+let minus = num1 - num2;
+let multiply = num1 * num2;
+let divide = num1 / num2;
+
+alert(` Result:
+    Sum: ${sum}
+    Minus: ${minus}
+    Multiply: ${multiply}
+    Divide: ${divide}
+`);
