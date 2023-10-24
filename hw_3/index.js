28729c1fa3112c6356c5f985363301a468f5fa86
MikhailV89 <114851579+MikhailV89@users.noreply.github.com>
2023-10-24 12:51:00 +0300

    Add files via upload

---
+++ hw_3/index.js
@@ -0,0 +1,10 @@
+let value = prompt('Write few numbers?');
+let newValue = value.toString().replaceAll('', ' ');
+console.log(newValue);
+
+let str1 = prompt('Write first string?');
+let str2 = prompt('Write second string?');
+let str3 = prompt('Write third string?');
+let result = `${str2} ${str1} ${str3}`;
+
+alert(result);
