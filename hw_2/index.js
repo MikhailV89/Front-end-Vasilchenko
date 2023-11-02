56730ce8ce31e7cc3229a8f760506c298e3a6122
MikhailV89 <114851579+MikhailV89@users.noreply.github.com>
2023-11-02 22:58:24 +0300

    Add files via upload

---
+++ hw_2/index.js
@@ -0,0 +1,7 @@
+const num1 = +prompt('Write first number?');
+const num2 = +prompt('Write second number?');
+const num3 = +prompt('Write third number?');
+
+const result = (num1 + num2 + num3) / 3;
+
+alert(`Result: ${result}`);
